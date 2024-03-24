4abf1660c087733f8b727ef104aa00fd6cb23483
Albatraoz12 <dimosthenis.emmanouil@chasacademy.se>
2024-03-24 19:49:52 +0100

    implement all states

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [answer, setAnswer] = useState(''); //Sets answer
+  const [isRight, setIsRight] = useState(false); //Display if user is right or wrong
+  const [message, setMessage] = useState(''); //Display message if user has it right or wrong
+  const colors = ['#9C334E', '#5607EE', '#C7DF67']; //Random colors to guess
+
   return (
     <>
       <div>
